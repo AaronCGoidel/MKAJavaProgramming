e596bcb67c83d293a124697e82cfc4eba26f7bed
AaronCGoidel <agoidel2019@mka.org>
2017-04-21 11:07:14 -0400

    Added some formatting

--- src/com/AaronGoidel/mka/fibonacci/FibonacciArrays.java
+++ src/com/AaronGoidel/mka/fibonacci/FibonacciArrays.java
@@ -1,5 +1,6 @@
 package com.AaronGoidel.mka.fibonacci;
 
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
@@ -9,12 +10,13 @@ public class FibonacciArrays
 {
     public static void main(String[] args)
     {
-        int lim = 10;
+        int lim = 20;
         long fibs[][] = new long[2][lim];
         int evenCount = 0;
         int oddCount = 0;
         int fibCount = 0;
         boolean go = true;
+
 
 
         while(go)
@@ -34,7 +36,7 @@ public class FibonacciArrays
             }
             fibCount++;
         }
-        System.out.println(Arrays.deepToString(fibs));
+        printer(fibs, lim);
 
     }
     public static long tailRecur(final int n)
@@ -45,5 +47,21 @@ public class FibonacciArrays
     private static long inner(final long a, final long b, final int n)
     {
         return n < 1 ? a : n == 1 ? b : inner(b, a + b, n - 1);
+    }
+
+    public static void printer(long list[][], int length)
+    {
+        PrintWriter out = new PrintWriter(System.out, false);
+        out.format("%23s%9s%10s\n", "Even", "||", "Odd");
+        for(int num = 0; num < length; num++)
+        {
+            out.format("%-9s%14d%9s%10d\n", "#" + (num+1), list[0][num], "||", list[1][num]);
+//            out.print(list[0][num]);
+//            out.print("||");
+//            out.print(list[1][num]);
+//            out.println();
+//
+        }
+        out.flush();
     }
 }
