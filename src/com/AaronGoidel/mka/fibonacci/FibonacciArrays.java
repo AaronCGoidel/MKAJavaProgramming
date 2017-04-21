f6212424c82a71ce335539e785cd3f53d3271c85
AaronCGoidel <agoidel2019@mka.org>
2017-04-21 12:07:11 -0400

    Commented Fibonacci code

--- src/com/AaronGoidel/mka/fibonacci/FibonacciArrays.java
+++ src/com/AaronGoidel/mka/fibonacci/FibonacciArrays.java
@@ -1,7 +1,6 @@
 package com.AaronGoidel.mka.fibonacci;
 
 import java.io.PrintWriter;
-import java.util.Arrays;
 
 /**
  * Created by agoidel2019 on 4/19/17.
@@ -10,8 +9,8 @@ public class FibonacciArrays
 {
     public static void main(String[] args)
     {
-        int lim = 20;
-        long fibs[][] = new long[2][lim];
+        int lim = 20; // how many of each number to generate
+        long fibs[][] = new long[2][lim]; // array that will store numbers
         int evenCount = 0;
         int oddCount = 0;
         int fibCount = 0;
@@ -19,26 +18,28 @@ public class FibonacciArrays
 
 
 
-        while(go)
+        while(go) // sort numbers
         {
-            long current = tailRecur(fibCount);
-            if(current % 2 == 0 & evenCount < lim)
+            long current = tailRecur(fibCount); // get next number in series
+            if(current % 2 == 0 & evenCount < lim) // check if even
             {
-                fibs[0][evenCount] = current;
+                fibs[0][evenCount] = current; // add to even sub-list
                 evenCount++;
             }else if(oddCount < lim)
             {
                 fibs[1][oddCount] = current;
                 oddCount++;
-            }else if(evenCount == oddCount)
+            }else if(evenCount == oddCount) // check if both lists are same length (meaning they are both full)
             {
-                go = false;
+                go = false; // set flag to false
             }
             fibCount++;
         }
-        printer(fibs, lim);
+        printer(fibs, lim); // run print
 
     }
+
+    // tail-recursive fibonacci calculator
     public static long tailRecur(final int n)
     {
         return inner(0, 1, n);
@@ -52,15 +53,11 @@ public class FibonacciArrays
     public static void printer(long list[][], int length)
     {
         PrintWriter out = new PrintWriter(System.out, false);
-        out.format("%23s%9s%10s\n", "Even", "||", "Odd");
+        out.format("%23s%9s%10s\n", "Even", "||", "Odd"); // format header
         for(int num = 0; num < length; num++)
         {
-            out.format("%-9s%14d%9s%10d\n", "#" + (num+1), list[0][num], "||", list[1][num]);
-//            out.print(list[0][num]);
-//            out.print("||");
-//            out.print(list[1][num]);
-//            out.println();
-//
+            out.format("%-9s%14d%9s%10d\n", "#" + (num+1), list[0][num], "||", list[1][num]); // format output
+
         }
         out.flush();
     }
